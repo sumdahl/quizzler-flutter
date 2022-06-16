288ba5038d2c2dbf9e2643274c6cef7d1d8ade26
Sumiran Dahal <sumdahl@pop-os.localdomain>
2022-06-16 10:09:42 +0545

    new changes

--- lib/main.dart
+++ lib/main.dart
@@ -25,16 +25,14 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [
-    Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
+  List<Icon> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
   ];
+  List<bool> answers = [false, true, true];
+  int questionNumber = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -47,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -69,15 +67,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               style: TextButton.styleFrom(backgroundColor: Colors.green),
+              //The user picked true
               onPressed: () {
-                //The user picked true
+              bool correctAnswer = answers[questionNumber];
+              if(correctAnswer == true){
+                print('You got it right');
+              } else {
+                print('You got it wrong');
+              }
                 setState(() {
-                  scoreKeeper.add(
-                    Icon(
-                      Icons.check,
-                      color: Colors.green,
-                    ),
-                  );
+                  questionNumber++;
                 });
               },
             ),
@@ -97,11 +96,23 @@ class _QuizPageState extends State<QuizPage> {
               style: TextButton.styleFrom(
                 backgroundColor: Colors.red,
               ),
-              onPressed: () {},
+              onPressed: () {
+                bool correctAnswer = answers[questionNumber];
+                if(correctAnswer == false){
+                  print('You got it right');
+                } else {
+                  print('You got it wrong');
+                }
+                setState(() {
+                  questionNumber++;
+                });
+              },
             ),
           ),
         ),
-        Row(children: scoreKeeper)
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
