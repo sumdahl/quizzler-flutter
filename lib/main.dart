7375c9a38e2605f965a6ba6ae6cceec332958f23
Sumiran Dahal <sumdahl@pop-os.localdomain>
2022-06-19 22:51:59 +0545

    New changes with Dart OOP

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -25,14 +28,10 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.',
-  ];
-  List<bool> answers = [false, true, true];
+
   int questionNumber = 0;
+
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -45,7 +44,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+               quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -69,7 +68,7 @@ class _QuizPageState extends State<QuizPage> {
               style: TextButton.styleFrom(backgroundColor: Colors.green),
               //The user picked true
               onPressed: () {
-              bool correctAnswer = answers[questionNumber];
+              bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
               if(correctAnswer == true){
                 print('You got it right');
               } else {
@@ -97,7 +96,7 @@ class _QuizPageState extends State<QuizPage> {
                 backgroundColor: Colors.red,
               ),
               onPressed: () {
-                bool correctAnswer = answers[questionNumber];
+                bool correctAnswer = quizBrain.questionBank[questionNumber].questionAnswer;
                 if(correctAnswer == false){
                   print('You got it right');
                 } else {
@@ -110,9 +109,9 @@ class _QuizPageState extends State<QuizPage> {
             ),
           ),
         ),
-        Row(
-          children: scoreKeeper,
-        )
+        // Row(
+        //   children: scoreKeeper,
+        // )
       ],
     );
   }
